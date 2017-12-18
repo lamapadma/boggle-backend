07c6f6d647e3e44130729f1a8a74ee866983c41a
stopvoldemort <david.squires1@gmail.com>
2017-12-18 10:53:58 -0500

    removed require_relative for the dictionary, as it is not needed

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -1,5 +1,3 @@
-# require_relative '../lib/dictionary.rb'
-#
 class RoundsController < ActionController::API
 
   def new
--- lib/dictionary.rb
+++ lib/dictionary.rb
@@ -1,5 +1,3 @@
-# require 'set'
-
 DICTIONARY = ["aardvark",
 "aardwolf",
 "aaron",
