9ee510dca3ed4ba2215ceec1d1f3acb3e23a3188
stopvoldemort <david.squires1@gmail.com>
2017-12-18 12:32:26 -0500

    debugging

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -15,7 +15,7 @@ class RoundsController < ActionController::API
   end
 
   def checkword
-    @test = true
+    @test = !!DOESNOTEXIST
     render json: @test
     # word = params[:word]
     # @response = DICTIONARY.include?(word)
